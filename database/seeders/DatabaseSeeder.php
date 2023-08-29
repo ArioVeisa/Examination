604c816bd050a4fea092aeac6e4524dfec48f35d
ArioVeisa <classlearning04g@gmail.com>
2023-08-29 10:32:59 +0700

    okay

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Database\Seeders\UserTableSeeder;
+use Database\Seeders\RolesTableSeeder;
+use Database\Seeders\PermissionsTableSeeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call(RolesTableSeeder::class);
+        $this->call(PermissionsTableSeeder::class);
+        $this->call(UserTableSeeder::class);
+        \App\Models\User::factory(10)->create();
+    }
+}
